4d72a9859415e43951f24aae707284e3b66c8ba6
Gagan2601 <110617326+Gagan2601@users.noreply.github.com>
2022-09-12 22:19:17 +0530

    Js models

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const full = player.querySelector('.fullscreen');
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+function updateButton(){
+    const icon = video.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon; 
+}
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+}
+function handleUpdate(){
+    const percent = (video.currentTime / video.duration)*100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth)*video.duration;
+    video.currentTime = scrubTime;
+}
+function fullscreen1(){
+    video.requestFullscreen();
+}
+
+video.addEventListener('click',togglePlay);
+video.addEventListener('play',updateButton);
+video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate',handleUpdate);
+toggle.addEventListener('click',togglePlay);
+full.addEventListener('click',fullscreen1);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click',scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
